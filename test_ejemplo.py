f01707ae3c37a5934d35e818c2fd0ae8064e3ebb
fragata-adrian <fragataadrian99@gmail.com>
2021-11-16 18:27:03 -0300

    cambie a pytest

--- test_ejemplo.py
+++ test_ejemplo.py
@@ -1,19 +1,16 @@
-import unittest
 from ejemplo import *
 
-class TestEjemplo(unittest.TestCase):
+def test_sumar():
+    assert sumar(15, 5) == 20
 
-    def test_sumar(self):
-        self.assertEqual(sumar(15, 5), 20)
+def test_restar():
+    assert restar(20, 5) == 15
 
-    def test_restar(self):
-        self.assertEqual(restar(20, 5), 15)
+def test_multiplicar():
+    assert multiplicar(5, 5) == 25
 
-    def test_multiplicar(self):
-        self.assertEqual(multiplicar(5, 5), 25)
+def test_dividir():
+    assert dividir(5, 2) == 2.5
 
-    def test_dividir(self):
-        self.assertEqual(dividir(5, 2), 2.5)
-
-    def test_dividirSinResto(self):
-        self.assertEqual(dividirSinResto(5, 2), 2)
+def test_dividirSinResto():
+    assert dividirSinResto(5, 2) == 2
